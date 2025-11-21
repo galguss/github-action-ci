fbeb7329e2290486db0f8b4fbfab86a17d6fc82a
galguss <gal.gus12@gmail.com>
2025-11-21 10:17:38 +0200

    tests

---
+++ src/math.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function doul(a){
+    return a % 2 == 0;
+}
+
+function p(a,b){
+    return a-b;
+}
+module.exports = {
+    sum,
+    doul,
+    p
+}
