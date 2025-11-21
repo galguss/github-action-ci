884923e2b2c7fb159f9d5acae75404b89b500cff
galguss <gal.gus12@gmail.com>
2025-11-21 10:55:02 +0200

    new test

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -1,5 +1,5 @@
 const { sum,doul,p } = require("../math");
 
 test("sum 2+3=5",() => expect(sum(2,3)).toBe(5));
-test("doul 6 % 2 == 0",() => expect(doul(6)).toBe(false));
+test("doul 6 % 2 == 0",() => expect(doul(6)).toBe(true));
 test("p 10 - 5 = 5",() => expect(p(10,5)).toBe(5));
